7b9e37950ac19a01b1331c9e150f4ae259232afa
王玕一 <872246150@qq.com>
2019-10-21 23:03:48 +0800

    Initial commit

---
+++ src/main/java/k1oud/career/wholecareer/WholecareerApplication.java
@@ -0,0 +1,13 @@
+package k1oud.career.wholecareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WholecareerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WholecareerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/k1oud/career/wholecareer/WholecareerApplicationTests.java
@@ -0,0 +1,13 @@
+package k1oud.career.wholecareer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WholecareerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
